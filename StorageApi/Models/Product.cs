c843b34e6bd24523c80f4e7dab4ad128b1b75890
wpplng <lina.wappling@gmail.com>
2025-06-26 13:49:29 +0200

    Added product model

---
+++ StorageApi/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace StorageApi.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Price { get; set; }
+        public string Category { get; set; } = string.Empty;
+        public string Shelf { get; set; } = string.Empty;
+        public int Count { get; set; }
+        public string Description { get; set; } = string.Empty; 
+
+    }
+}
